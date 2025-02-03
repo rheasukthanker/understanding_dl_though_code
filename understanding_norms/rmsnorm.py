060c987515e3ac5fadb673c309bf40eeb073a85c
RHEA SUKTHANKER <sukthank@RHEAs-MacBook-Pro-2.local>
2025-02-03 02:37:42 +0100

    fix rmsnorm

--- understanding_norms/rmsnorm.py
+++ understanding_norms/rmsnorm.py
@@ -13,13 +13,12 @@ class MyRMSNorm(torch.nn.Module):
             self.gamma = torch.nn.Parameter(torch.ones(num_features))
 
     def forward(self, x):
-        assert len(x.shape) == 3
         assert x.shape[-1] == self.num_features
         rms = torch.sqrt(torch.mean(x**2, dim=-1, keepdim=True) + self.eps)
         if self.elementwise_affine:
             return x / rms * self.gamma
         else:
-            return x
+            return x/rms
 
     def eval(self):
         self.training = False
@@ -32,7 +31,7 @@ if __name__ == "__main__":
     batch_size = 32
     num_features = 128
     myrms = MyRMSNorm(num_features, elementwise_affine=True, eps=1e-09)
-    inp = torch.randn([batch_size, 4, num_features])
+    inp = torch.randn([batch_size, num_features])
     rms = torch.nn.RMSNorm(num_features, elementwise_affine=True, eps=1e-09)
     assert torch.allclose(myrms(inp), rms(inp), atol=1e-05)
     myrms.eval()
